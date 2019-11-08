5fc99015c68e28e7900f128000d64b7fdf537efe
mohitgarg <mohitgarg083@gmail.com>
2019-11-08 11:59:35 +0530

    lec106

--- pics/src/components/imagecard.js
+++ pics/src/components/imagecard.js
@@ -1,9 +1,24 @@
 import React from "react"
 
 class ImageCard extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={spans:0}
+        this.ImageRef=React.createRef()
+    }
+    componentDidMount(){
+        // console.log(this.ImageRef.current.clientHeight)
+        const spans=Math.ceil(this.ImageRef/150+1)
+        this.setState({spans})
+    }
+
     render(){
         const{description,urls}=this.props.image
-        return<img  alt={description} src={urls.regular}></img>
+        return (
+            <div style={{gridRowEnd:`span ${this.state.spans}`}}>
+                <img ref={this.ImageRef} alt={description} src={urls.regular}></img>
+            </div>
+        )
     }
 }
 export default ImageCard
